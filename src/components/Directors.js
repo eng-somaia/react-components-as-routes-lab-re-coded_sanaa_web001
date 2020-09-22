faafc5e19b10e18bcddff73f67f7644b585e4baf
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-22 21:06:32 +0000

    Automatically backed up by Learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -21,17 +21,4 @@ const Directors = () => {
 }
 
 export default Directors
-let directors = [
-  {
-    name: 'Scott Derrickson',
-    movies: ['Doctor Strange', 'Sinister', 'The Exorcism of Emily Rose']
-  },
-  {
-    name: 'Mike Mitchell',
-    movies: ['Trolls', 'Alvin and the Chipmunks: Chipwrecked', 'Sky High']
-  },
-  {
-    name: 'Edward Zwick',
-    movies: ['Jack Reacher: Never Go Back', 'Blood Diamon', 'The Siege']
-  }
-]
+
