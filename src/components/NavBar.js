e60368870bdfa4bdf88fc9e16c3b8ac2d34b1d2e
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-22 21:14:22 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className="navbar">
-      <NavLink to="/" exact />
-      <NavLink to="/movies" exact />
-      <NavLink to="/directors" exact />
-      <NavLink to="/actors" exact />
+      <NavLink to="/" exact />Home</NavLink>
+      <NavLink to="/movies" exact />Movies</NavLink>
+      <NavLink to="/directors" exact />Directors</NavLink>
+      <NavLink to="/actors" exact />Actors</NavLink>
     </div>
   );
 };
