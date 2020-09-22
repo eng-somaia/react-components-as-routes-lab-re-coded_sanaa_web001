5bf61e6d21f5d50762bd7c9ab2e2ac1ec81b4cd8
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-22 21:06:22 +0000

    Automatically backed up by Learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,9 +4,34 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      directors.map(ele=>{
+        <div>
+           <h3>`Name: ${ele.name}`</h3>
+           <p>'Movies: '</p>
+           ele.movies.map(item =>{
+             <ul>
+               <li>item</li>
+             </ul>
+           })
+        </div>
+      })
     </div>
   );
 }
 
 export default Directors
+let directors = [
+  {
+    name: 'Scott Derrickson',
+    movies: ['Doctor Strange', 'Sinister', 'The Exorcism of Emily Rose']
+  },
+  {
+    name: 'Mike Mitchell',
+    movies: ['Trolls', 'Alvin and the Chipmunks: Chipwrecked', 'Sky High']
+  },
+  {
+    name: 'Edward Zwick',
+    movies: ['Jack Reacher: Never Go Back', 'Blood Diamon', 'The Siege']
+  }
+]
