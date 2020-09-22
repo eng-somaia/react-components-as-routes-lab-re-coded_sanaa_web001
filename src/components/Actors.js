1a2a3bd068298cbecee7b524286b603bf24e3bb0
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-22 21:09:57 +0000

    Automatically backed up by Learn

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -5,11 +5,11 @@ const Actors = () => {
   return (
     <div>
     <h1>Actors Page</h1>
-    actors.map(ele=>{
+    actors.map(ele => {
       <div>
          <h3>`Name: ${ele.name}`</h3>
          <p>'Movies: '</p>
-         ele.movies.map(item =>{
+         ele.movies.map(item => {
            <ul>
              <li>item</li>
            </ul>
